2a6c52310fd46ea925b58729fb51df4d4612eaeb
luis.gfsilva <134225065+LuisGuSilva@users.noreply.github.com>
2025-06-30 16:46:48 -0500

    c

--- scripts/Js.js
+++ scripts/Js.js
@@ -26,18 +26,39 @@ function toggleNav() {
     });
   }
 }
-/* 
-   const input = document.getElementById('searchInput');
-    const content = document.getElementById('content');
-    const originalText = content.innerHTML;
 
-    input.addEventListener('input', () => {
-      const query = input.value.trim();
-      if(!query) {
-        content.innerHTML = originalText;
-        return;
+function SearchMark(inputId, contentId) {
+      const input = document.getElementById(inputId);
+      const content = document.getElementById(contentId);
+      const originalText = content.innerHTML;
+
+      input.addEventListener('input', () => {
+        const query = input.value.trim();
+
+        if (!query) {
+          content.innerHTML = originalText;
+          return;
+        }
+
+        const escapedQuery = query.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        const regex = new RegExp(`(${escapedQuery})`, 'gi');
+
+        content.innerHTML = originalText.replace(regex, '<mark>$1</mark>');
+      });
+}
+
+    
+    setupLiveSearchHighlight('searchInput', 'content');
+	
+document.addEventListener('DOMContentLoaded',function(){
+	function detectarDispositivo() {
+      const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+      const x = document.querySelector('container');
+    if(/android/i.test(userAgent)){
+      
+	  }else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        x.style.fontSize = '16px'
       }
-      const regex = new RegExp(`(${query.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})`, 'gi');
-      content.innerHTML = originalText.replace(regex, '<mark>$1</mark>');
-    });
-*/ 
+    }
+    detectarDispositivo();
+  });
